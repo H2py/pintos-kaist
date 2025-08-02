3888d79a4c56d07b7b15a1dc7e7f98e1d4b095c7
양진성 <118969533+omokgosu@users.noreply.github.com>
2025-08-02 23:56:55 +0900

    [refactor] remove exec-sema (#3)
    remove exec palloc get page
    remove file duplicate

--- pintos/userprog/syscall.c
+++ pintos/userprog/syscall.c
@@ -195,9 +195,8 @@ tid_t exec(const char *cmd_line)
     if(file_copy == NULL) return -1;
     
     strlcpy(file_copy, cmd_line, PGSIZE);
-    result = process_exec(file_copy);
     
-    palloc_free_page(file_copy);
+    result = process_exec(file_copy);
 
     return result;
 }
